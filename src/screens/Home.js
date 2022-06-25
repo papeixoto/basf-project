9aedcca32fe88f36208c24d5f639c1bbc9b8d593
mariajoaosantos <mariajcs18@gmail.com>
2022-06-25 20:50:49 +0100

    styles for home added

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,15 @@
 import { Text, Image, View } from "react-native";
+import { StyleSheet } from "react-native-web";
 
 export default function Home() {
   return (
     <View>
-      <Image source={require("../assets/basf-logo.png")} />
-      <Text className="homeTitle">Discover more about you</Text>
-      <Text className="homeDescription">
+      <Image
+        source={require("../assets/basf-logo.png")}
+        style={{ width: 299, height: 373 }}
+      />
+      <Text style={styles.title}>Discover more about you</Text>
+      <Text style={styles.description}>
         Have you ever wondered what does your daily life products contain? What
         are you exactly buing? With BASF app you can find out this and much
         more!
@@ -13,3 +17,12 @@ export default function Home() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 22,
+  },
+  description: {
+    fontSize: 16,
+  },
+});
